3e52acc88cb14e7ed8b17910d3b1f6e4c1eeae22
Siarzis <siarziss@gmail.com>
2018-09-17 15:50:11 +0300

    total black box

--- find_missing_values.py
+++ find_missing_values.py
@@ -1,82 +1,76 @@
 import re
+import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-def find_missing_values(file, threshold_1, threshold_2):
+def find_missing_values(input_series, gen_threshold, speed_threshold, pv_threshold, attribute):
 
-    pv_threshold = float('inf')
-    # file contains the path to the chosen .csv
-    df = pd.read_csv(file)
-
-    if file == 'PV Info/PvData.csv' and threshold_2 == float('inf'):
-        attribute = 'PvPower'
-        pv_threshold = 12
-    elif file == 'PV Info/PvData.csv' and threshold_2 != float('inf'):
-        print('Invalid input parameters! Please try again.')
-        return
-    else:
-        if threshold_2 == float('inf'):  # only direction
-            attribute = 'WindDirection'
-        else:
-            attribute = 'WindSpeed'
+    # input_series contains historical weather data (wind speed/wind direction, etc)
+    df = input_series.to_frame()
+    df.columns = [attribute[1]]
 
     # complex, one-line commands that do all the work. Unfortunately this is the way pandas do the job
     # found after search on StackOverflow
     # 'block' term is introduced in order to distinguish the different intervals in which
     # the same value can be continuously repeated
-    df['block'] = (df[attribute].shift(1) != df[attribute]).astype(int).cumsum()
+    df['block'] = (df[attribute[1]].shift(1) != df[attribute[1]]).astype(int).cumsum()
     # consecutive_intervals is a pandas Series that contain all intervals of interest
-    consecutive_intervals = df.reset_index().groupby([attribute, 'block'])['index'].apply(np.array)
+    consecutive_intervals = df.reset_index().groupby([attribute[1], 'block'])['index'].apply(np.array)
 
     # keep a copy of original Series to plot both original and updated versions
-    initial_wind_farm = df.copy()
-    initial_time_series = pd.Series(initial_wind_farm[attribute].values, index=df['Dates'])
+    df_copy = df.copy()
+    initial_time_series = pd.Series(df_copy[attribute[1]].values, index=df.index)
 
     # consecutive_intervals variables break into tuple and list with proper values
     # for iteration and accessing reasons
     for value_block_tuple, indexes in consecutive_intervals.items():
         indexes = indexes.tolist()  # turn array to list
         # if measurements give the same value for 4  hours and above then they are not accepted
-        if len(indexes) > threshold_1 and value_block_tuple[0] != 0:  # TODO check for this condition
-            for elem in indexes:
-                df.at[elem, attribute] = float('nan')
+        if pv_threshold != float('inf'):
+            if len(indexes) > gen_threshold and value_block_tuple[0] > 0.5:
+                for elem in indexes:
+                    df.at[elem, attribute[1]] = float('nan')
+        else:
+            if len(indexes) > gen_threshold and value_block_tuple[0] != 0:
+                for elem in indexes:
+                    df.at[elem, attribute[1]] = float('nan')
         # contrary to previous one, if measurements are zero for 10 hours and above, then they are not accepted
         # this is reasonable because no windy conditions may occur
-        if len(indexes) > threshold_2 and value_block_tuple[0] == 0:
+        if len(indexes) > speed_threshold and value_block_tuple[0] == 0:
             for elem in indexes:
-                df.at[elem, attribute] = float('nan')
+                df.at[elem, attribute[1]] = float('nan')
         if len(indexes) > pv_threshold and value_block_tuple[0] <= 0.5:
             for elem in indexes:
-                df.at[elem, attribute] = float('nan')
+                df.at[elem, attribute[1]] = float('nan')
 
-    time_series = pd.Series(df[attribute].values, index=df['Dates'])
+    time_series = pd.Series(df[attribute[1]].values, index=df.index)
 
     initial_time_series.plot()
     time_series.plot()
     plt.show()
 
-
-while True:
-
-    while True:
-        filename = input("Please enter your file's name: ")
-        if re.match(r"Wind Info/WindFarm[1-5].csv$|PV Info/PvData.csv$", filename):  # TODO regex may need modification
-            break
-        else:
-            print("Sorry, your response does not match proper input! Please try again.")
-            continue
-
-    while True:
+    if pv_threshold != float('inf'):
+        filename = 'Fixed PV Info/' + attribute[1] + '.csv'
+    else:
+        filename = 'Fixed Wind Info/' + attribute[1] + str(attribute[0]) + '.csv'
+    if os.path.exists(filename):
         try:
-            parameter_1 = int(input("Please enter general parameter value: "))
-        except ValueError:
-            print("Sorry, I didn't understand that.")
-            continue
-        else:
-            break
+            os.remove(filename)
+        except OSError as e:
+            print("Error: %s - %s." % (e.filename, e.strerror))
+    df.drop(columns=['block'], inplace=True)  # delete unnecessary columns
+    df.to_csv(filename)
 
-    parameter_2 = float(input("Please enter wind speed parameter value: "))
 
-    find_missing_values(filename, parameter_1, parameter_2)
+for k in range(0, 5):
+    farm_data = pd.read_csv('Wind Info\WindFarm' + str(k+1) + '.csv')
+    direction_time_series = pd.Series(farm_data['WindDirection'].values, index=farm_data.index)
+    find_missing_values(direction_time_series, 4, float('inf'), float('inf'), [k+1, 'WindDirection'])
+    speed_time_series = pd.Series(farm_data['WindSpeed'].values, index=farm_data.index)
+    find_missing_values(speed_time_series, 4, 10, float('inf'), [k+1, 'WindSpeed'])
+
+pv_data = pd.read_csv('PV Info\PvData.csv')
+pv_time_series = pd.Series(pv_data['PvPower'].values, index=pv_data.index)
+find_missing_values(pv_time_series, 4, float('inf'), 12, ['-', 'PvPower'])
